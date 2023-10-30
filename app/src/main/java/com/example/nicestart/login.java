b2e428fa14bbe25a55a428e3d6f7c5e0ddd54929
arturofgg <arturofdezgg17@gmail.com>
2023-10-30 12:49:17 +0100

    add androidManifest lines

--- app/src/main/java/com/example/nicestart/login.java
+++ app/src/main/java/com/example/nicestart/login.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginConstraining extends AppCompatActivity {
+public class login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_constraining);
+        setContentView(R.layout.activity_login);
     }
 }
