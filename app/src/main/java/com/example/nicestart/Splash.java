147cb765c551ff1827a1663dd5cf4455b6bfaadf
arturofgg <arturofdezgg17@gmail.com>
2023-11-15 14:02:44 +0100

    animaciones y splash

--- app/src/main/java/com/example/nicestart/Splash.java
+++ app/src/main/java/com/example/nicestart/Splash.java
@@ -1,13 +1,18 @@
 package com.example.nicestart;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class Splash extends AppCompatActivity {
@@ -16,18 +21,41 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        openApp();
+
+        //implements and starts animation
+        // objeto thunder sobre el cual aplicaremos la animación
+        ImageView thunder = findViewById(R.id.logosplash);
+
+        //creamos un objeto animación que incorpora la animación descrita en el xml y con el método
+        // startAnimation lo aplicamos al imageview del logo
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.mixedanimation);
+        thunder.startAnimation(myanim);
 
         //    Glide for loading girls
         ImageView mSea = findViewById(R.id.backView);
-
         Glide.with(this)
-                .load("https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80")
+                .load("https://i.pinimg.com/originals/ed/7a/8c/ed7a8cbae40fe647587bcf5f39020f38.gif")
 //                .load(R.drawable.girl)
                 .transition(DrawableTransitionOptions.withCrossFade(100))
                 .centerCrop()
-//                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .fitCenter()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
-//                .circleCrop()
                 .into(mSea);
+    }
+    private void openApp() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable()
+        {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash
+                        .this, login.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        }, 5000);
     }
 }
